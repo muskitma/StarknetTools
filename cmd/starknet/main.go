afd4ed3331e8384f643046135a8f0b1a39c88e04
Muski <143282825+muskitma@users.noreply.github.com>
2025-12-14 23:08:07 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/starknet/main.go
@@ -0,0 +1,23 @@
+// cmd/starknet/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "starknet/internal/starknet"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := starknet.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
